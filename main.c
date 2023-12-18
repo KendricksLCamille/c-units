27f350e21b0b2434c48fa384a60e7aff2987181f
kendricks <camilles20049@hotmail.com>
2023-12-18 02:12:46 -0500

    adding ohms

--- main.c
+++ main.c
@@ -6,10 +6,13 @@
 #include <stdio.h>
 
 #include "units_quantity.h"
+#include "units_dimmension.h"
 int main()
 {
-    units_si_quantity* created = units_si_quantity_create(1, &METER);
+    /*units_si_quantity* created = units_si_quantity_create(1, &METER);
     units_si_quantity* added = unit_add(created, created);
-    printf("%s\n", unit_to_string_MALLOC(added));
+    printf("%s\n", unit_to_string_MALLOC(added));*/
+
+    printf("%s\n", unit_to_string_MALLOC(&OHM));
     return 0;
 }
